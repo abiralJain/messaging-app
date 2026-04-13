1bc096c903b6c91493a45b219be31fe7b35aa1b3
abiralJain <abiral1212.ab@gmail.com>
2026-04-13 20:07:28 +0530

    Initial Commit

---
+++ Messaging app/Messaging_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messaging_appApp.swift
+//  Messaging app
+//
+//  Created by Abiral Jain on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Messaging_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
